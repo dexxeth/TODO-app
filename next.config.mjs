cfb89e821a7c4125029934c58794381544f2d95f
dexxeth <anshgup.9450@gmail.com>
2024-07-15 11:55:45 +0530

    Configure deployment to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withVideos from "next-videos";
 
 const nextConfig = {
 	output: "export",
+	basePath: process.env.NODE_ENV === "production" ? "/nextjsmarketplace" : "",
 };
 
 export default {
